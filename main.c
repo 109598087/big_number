79508999eba3af54ac966b8555ce8db2853035e3
109598087 <t109598087@ntut.org.tw>
2020-09-02 23:34:06 +0800

    同號OK 異號還不能

--- main.c
+++ main.c
@@ -8,7 +8,6 @@ void Sub(char input1[], char input2[], char c[])
 
 for(i = 0;i<LEN;i++)
  {
-
      re_input1[i] = '0';
      re_input2[i] = '0';
      c[i] = '0';
@@ -23,9 +22,79 @@ for(i = 0;i<LEN;i++)
  {
      re_input2[i] = input2[len2-i-1];
  }
- Print(re_input1);
- printf("\n");
- Print(re_input2);
+
+ // 判斷正負號
+ int input1_m = 0, input2_m = 0;
+ for(i=0;i<LEN;i++)
+ {
+     if(re_input1[i] == '-')
+        input1_m += 1;
+     if(re_input2[i] == '-')
+        input2_m += 1;
+ }
+ if(len1 > len2)
+ {
+    if(input1_m == input2_m)
+    {
+     for(i=0;i<LEN;i++)
+     {
+         if(re_input1[i] == '-')
+         {
+             c[i] = re_input1[i];
+         }
+         else if(re_input2[i] == '-')
+         {
+             c[i] = re_input1[i];
+         }
+         else
+             c[i] = re_input1[i] - re_input2[i]+'0';
+         if(re_input1[i] == '0'&& re_input2[i] == '0') //有問題
+            break;
+
+     }
+     int temp[LEN] = {0};
+     for(i=0;i<LEN;i++)
+     {
+         if(c[i] == '-')
+         {
+             temp[i] = 100;
+             break;
+         }
+
+         temp[i] = c[i] - '0';
+
+     }
+
+     for(i=0;i<LEN-1;i++)
+     {
+         if(temp[i]<0)
+         {
+             temp[i+1] -= 1;
+             temp[i] = 10+temp[i];
+         }
+
+     }
+     for(i=0;i<LEN;i++)
+     {
+         if(temp[i] == 100)
+         {
+             c[i] = '-';
+             break;
+         }
+         c[i] = temp[i] + '0';
+     }
+
+
+    }
+
+
+    else
+    {
+     printf("異號");
+    }
+ }
+
+
 
 
 
@@ -53,9 +122,19 @@ void Add(char input1[], char input2[], char c[])
  {
      re_input2[i] = input2[len2-i-1];
  }
- int temp[LEN];
+ // 判斷是否有負號
+ int input1_m = 0, input2_m = 0;
  for(i=0;i<LEN;i++)
  {
+     if(re_input1[i] == '-')
+        input1_m += 1;
+     if(re_input2[i] == '-')
+        input2_m += 1;
+ }
+ if(input1_m ==  input2_m){ //同號
+
+    for(i=0;i<LEN;i++)
+    {
      if(re_input1[i] == '-' )
      {
           if(len1>len2)
@@ -72,10 +151,18 @@ void Add(char input1[], char input2[], char c[])
      }
      else
         c[i] = re_input1[i] + re_input2[i]-'0';
-     if(re_input1[i] == '0' && re_input2[i] == '0')
+     if(re_input1[i] == '0' && re_input2[i] == '0') // 這裡有問題
         break;
 
+    }
+ }else
+ {
+     printf("異號");
  }
+
+
+
+ int temp[LEN];
  for(i=0;i<LEN;i++)
  {
      temp[i] = c[i] - '0';
@@ -131,12 +218,11 @@ void Print(char n[])
 
 void main()
 {
- char input1[] = "-1234999", input2[] = "-66699", output[LEN];
+ char input1[] = "999999", input2[] = "999", output[LEN];
+ int i;
 
-
-
- Add(input1, input2, output);
- //Sub(input1, input2, output);
+ // Add(input1, input2, output);
+ Sub(input1, input2, output);
 
  re_Print(output);
 
